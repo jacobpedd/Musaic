1509d30481966a32f34056c42e8444f6f12bcecd
Jacob Peddicord <jpmail17@gmail.com>
2018-12-17 12:10:52 -0600

    Using Spotify redirect and trying to detect access token

---
+++ main.js
@@ -0,0 +1,29 @@
+SPOTIFY_CLIENT_ID = "8e1781d096804201b4688932dde7e873";
+
+var spotifyApi = new SpotifyWebApi();
+
+// See if we have a token stored
+let token = localStorage.getItem('spotify_token');
+if (token !== null) {
+    console.log("verified");
+    // Test the token
+}
+
+// See if there is a token in the URL
+
+// Get User Access Token
+const callback_url = encodeURIComponent('https://serene-curran-915888.netlify.com');
+const url = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${callback_url}&scope=user-library-read&response_type=token`;
+document.getElementById('spotify-link').href = url;
+
+// Display Result
+let resultDiv = document.getElementById('result');
+if (window.location.href.includes('#access_token')) {
+    console.log('token in url');
+    resultDiv.innerText = "token";
+} else {
+    console.log('no token in url');
+    resultDiv.innerText = "no token";
+}
+
+console.log(window.location.href);
